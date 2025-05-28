aa0dcc30cde9e6c8ea61ed0ecb5e42d480f09d1a
daseru <107600857+daseru0702@users.noreply.github.com>
2025-05-28 15:41:13 +0900

    Update AdapterFactory.js

--- common/AdapterFactory.js
+++ common/AdapterFactory.js
@@ -1,22 +1,13 @@
 // common/AdapterFactory.js
 
 export class AdapterFactory {
-    /**
-     * @param {string} gameName  ex: "quoridor"
-     * @param {object} stateJson raw JSON state from client or null for new game
-     */
-    static async create(gameName, stateJson = null) {
-      switch (gameName) {
-        case "quoridor": {
-          const { QuoridorAdapter } = await import(
-            "../games/quoridor/QuoridorAdapter.js"
-          );
-          return new QuoridorAdapter(stateJson);
-        }
-        // add cases for other games here...
-        default:
-          throw new Error(`Unknown game: ${gameName}`);
-      }
-    }
+  static async create(gameName, config) {
+    const capitalize = name => name.charAt(0).toUpperCase() + name.slice(1);
+
+    const capName   = capitalize(gameName);
+    const modulePath = `../games/${gameName}/${capName}Adapter.js`;
+    const adapterMod = await import(modulePath);
+    const AdapterCls = adapterMod[`${capName}Adapter`];
+    return new AdapterCls(config);
   }
-  
+}
